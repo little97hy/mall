f5f106a4085da913cf6494ee3d3b1304d435e788
hy <805407908@qq.com>
2023-04-04 08:54:46 +0800

    修改

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: hy 805407908@qq.com
+ * @Date: 2023-03-31 18:13:40
+ * @LastEditors: hy 805407908@qq.com
+ * @LastEditTime: 2023-04-03 18:01:18
+ * @FilePath: \cloud_prod:\前端项目\mall\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # mall
 
 ## Project setup
@@ -22,3 +30,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+```
+4-3
+划分目录结构
+引用俩css文件
+
+```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
+<!--
+ * @Author: hy 805407908@qq.com
+ * @Date: 2023-03-31 18:13:40
+ * @LastEditors: hy 805407908@qq.com
+ * @LastEditTime: 2023-04-03 17:30:25
+ * @FilePath: \cloud_prod:\前端项目\mall\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
